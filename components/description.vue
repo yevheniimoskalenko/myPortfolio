3b5ae42221f01ef13c985e18fe88c0ade00fcefd
testjokerqwerty@gmail.com <testjokerqwerty@gmail.com>
2021-05-01 07:43:02 +0300

    add description

---
+++ components/description.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="description">
+    <div class="brand">
+      <img src="~/assets/image/10.png" alt="" />
+    </div>
+    <h2>What is Businesswise</h2>
+    <p>
+      Businesswise freelance was a project where I worked as a designer. The
+      main goal of businesswise is to gather podcasts in various categories for
+      professional people.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+.description {
+  width: 45%;
+  h2 {
+    margin-top: 8px;
+  }
+  p {
+    margin-top: 8px;
+    line-height: 26px;
+  }
+  .brand {
+    margin-top: 45px;
+    img {
+      border-radius: 6px;
+    }
+  }
+}
+</style>
