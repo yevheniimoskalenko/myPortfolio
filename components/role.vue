27bf40b9d1953a036eaeaab242c056727f147ff2
testjokerqwerty@gmail.com <testjokerqwerty@gmail.com>
2021-05-01 07:59:09 +0300

    role

---
+++ components/role.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="roles">
+    <el-row :gutter="30">
+      <el-col :xs="24" :sm="8" :md="8" :lg="8" :xl="8"
+        ><div class="card mr-1 none">
+          <h3>Team</h3>
+          <p>Vadi Efe - Founder Metehan Kaya - Product Designer</p>
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="8" :md="8" :lg="8" :xl="8"
+        ><div class="card mr-1">
+          <h3>My Role</h3>
+          <p>
+            I worked as a freelance Product Designer in the Businesswise
+            project.
+          </p>
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="8" :md="8" :lg="8" :xl="8"
+        ><div class="card mr-1">
+          <h3>Goals</h3>
+          <p>
+            Bringing professional people together with various categories of
+            podcasts.
+          </p>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+.roles {
+  margin-top: 128px;
+}
+</style>
--- pages/study/_id.vue
+++ pages/study/_id.vue
@@ -3,6 +3,7 @@
     <el-col :xs="24" :sm="20" :md="18" :lg="18" :xl="18">
       <app-header />
       <app-description />
+      <app-role />
     </el-col>
   </el-row>
 </template>
@@ -10,8 +11,9 @@
 <script>
 import appHeader from '@/components/header'
 import appDescription from '@/components/description'
+import appRole from '@/components/role'
 export default {
-  components: { appHeader, appDescription },
+  components: { appHeader, appDescription, appRole },
 }
 </script>
 
