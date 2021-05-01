47d90e64732767766b8bc746c87b90754ea81556
testjokerqwerty@gmail.com <testjokerqwerty@gmail.com>
2021-05-01 08:12:29 +0300

    add components preview

---
+++ components/preview.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="preview">
+    <el-row :gutter="30">
+      <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24"
+        ><div class="card mr-2 none">
+          <img src="~/assets/image/12.jpg" alt="" /></div
+      ></el-col>
+      <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24"
+        ><div class="card mr-2">
+          <img src="~/assets/image/13.jpg" alt="" /></div
+      ></el-col>
+      <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24"
+        ><div class="card mr-2">
+          <img src="~/assets/image/14.jpg" alt="" /></div
+      ></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+.preview {
+  margin-top: 128px;
+}
+.card {
+  img {
+    width: 100%;
+  }
+}
+</style>
--- pages/study/_id.vue
+++ pages/study/_id.vue
@@ -4,6 +4,8 @@
       <app-header />
       <app-description />
       <app-role />
+      <app-portfolio :show-link="false" />
+      <app-preview />
     </el-col>
   </el-row>
 </template>
@@ -12,8 +14,10 @@
 import appHeader from '@/components/header'
 import appDescription from '@/components/description'
 import appRole from '@/components/role'
+import appPortfolio from '@/components/portfolio'
+import appPreview from '@/components/preview'
 export default {
-  components: { appHeader, appDescription, appRole },
+  components: { appHeader, appDescription, appRole, appPreview, appPortfolio },
 }
 </script>
 
